4eeee4475f785871d1f3c65c34555f9757e3a890
Qurious-BattleTank <93530866+Qurious-BattleTank@users.noreply.github.com>
2021-11-01 22:46:45 +0900

    Create PrimeNumber.py

---
+++ PrimeNumber.py
@@ -0,0 +1,15 @@
+M=int(input('상한을 입력하세요 : '))
+
+def is_prime(n):
+    for i in p:
+        if n%i==0:
+            return False
+    return True
+
+p=[]
+count=0
+for k in range(2, M+1):
+    if is_prime(k)==True:
+        p.append(k)
+        count+=1
+print('%d 이하 소수의 개수는 %d개' %(M, count))
